4de93e813003f2f934a39d71bd787af97d1c5369
Jose Reimondez <josereimondez29@gmail.com>
2024-08-17 11:44:22 +0000

    corrigiendo wsgi

--- portofolio_web/portofolio_web/wsgi.py
+++ portofolio_web/portofolio_web/wsgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myportfolio.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portofolio_web.settings')
 
 application = get_wsgi_application()
+
