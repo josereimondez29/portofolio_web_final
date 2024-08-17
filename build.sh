b345c1106c5cc70d71a7871f16989a44121991a5
Jose Reimondez <josereimondez29@gmail.com>
2024-08-17 11:39:10 +0000

    corrigiendo build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -o errexit
 pip install -r requirements.txt
 
 # Convert static asset files
-python manage.py collectstatic --no-input
+python portofolio_web/manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python manage.py migrate
+python portofolio_web/manage.py migrate
