a0eb5a8feb05fdb110838a0455d612708531c3c3
Jose Reimondez <josereimondez29@gmail.com>
2025-08-02 12:00:46 +0200

    eliminacion de django

--- data/bioData.js
+++ data/bioData.js
@@ -1,4 +1,4 @@
 export const bioData = {
-  descOne: `Desarrollador de Software Full Stack, con experiencia en liderazgo y trabajo en equipo, excelente capacidad comunicativa y actualmente trabajando en proyectos con tecnologías como HTML, CSS, JavaScript, React, Python SQL y Flask.`,
-  descTwo: `Soy un hombre dispuesto a trabajar. Cuento con la capacidad de Planificar y Organizar procesos con destrezas para el cumplimiento de las tareas asignadas facilidad para la comunicacion oral y escrita. Poseo un alto sentido de responsabilidad recursividad, creatividad para cumplir con el deber asignado y facil adaptacion a los cambios con el compromiso de seguir a cabalidad las politicas corporativas apoyadas en mi formacion profesional y personal.`,
+  descOne: `Desarrollador Python con amplia experiencia en desarrollo de software, automatización de procesos y análisis de datos. Experto en frameworks como Flask, Django y FastAPI para la creación de APIs RESTful, así como en herramientas de ciencia de datos como Pandas, NumPy, Pyspark, Kafka y bibliotecas de aprendizaje automático.`,
+  descTwo: `Gran conocimiento en contenedores, CI/CD y despliegue en la nube. Experiencia en desarrollo backend y full stack, con un enfoque orientado a resultados y optimización de sistemas mediante automatización y soluciones basadas en datos. Excelentes habilidades de liderazgo y comunicación.`,
 };
--- data/profileInfo.js
+++ data/profileInfo.js
@@ -2,8 +2,8 @@
 
 
 export const profileInfo = {
-   name:'José Reimondez',
-   title:'Full Stack Developer',
+   name:'JOSÉ REIMONDEZ',
+   title:'Desarrollador Python & Científico de Datos',
    imageSrcOne:'/assets/img/parsonal-info/parson-img-1.png',
    imageSrcTwo:'/assets/img/parsonal-info/parson-img-3.png',
    imageSrcThree:'/assets/img/parsonal-info/parson-img-2.png',
